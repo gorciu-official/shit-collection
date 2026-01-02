f94cbfeecfe65d48be0801e284961008daa69229
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2026-01-02 18:42:52 +0100

    final build script

--- tooling/downloads/ghlac.sh
+++ tooling/downloads/ghlac.sh
@@ -1,3 +1,1 @@
-wget --no-verbose https://github.com/gorciu-official/ghla/releases/download/beta3/ghlac.elf -O ~/.cache/ghlac
-sudo mv ~/.cache/ghlac /usr/local/bin/ghlac
-sudo chmod 755 /usr/local/bin/ghlac
+wget --no-verbose https://github.com/gorciu-official/ghla/releases/download/beta3/ghlac.elf -O ~/.cache/ghlac && sudo mv ~/.cache/ghlac /usr/local/bin/ghlac && sudo chmod 755 /usr/local/bin/ghlac
